8683e15ccadd380f698f4a58ff2664d289df5f47
Sourish <nit_sourish@rediffmail.com>
2021-01-16 20:16:35 +0100

    Create utils_functions.py

---
+++ utils_functions.py
@@ -0,0 +1,106 @@
+!pip install tf-nightly-2.0-preview
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint,ReduceLROnPlateau
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+import matplotlib.pylab as plt
+from sklearn.metrics import mean_squared_error
+print(tf.__version__)
+
+#Time series generation function
+
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    return np.where(season_time < 0.1,
+                    np.cos(season_time * 6 * np.pi),
+                    2 / np.exp(9 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+    
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(False)
+
+#Time series forecasting
+def moving_average_forecast(series, window_size):
+    forecast = []
+    for time in range(len(series) - window_size):
+        forecast.append(series[time:time + window_size].mean())
+    return np.array(forecast)
+
+def ARIMA_forecast(x_train,x_valid):
+    history = [x for x in x_train]
+    predictions = list()
+    for t in range(len(x_valid)):
+        model = ARIMA(history, order=(2,1,1))
+        model_fit = model.fit()
+        output = model_fit.forecast()
+        yhat = output[0]
+        predictions.append(yhat)
+        obs = x_valid[t]
+        history.append(obs)
+        # print('predicted=%f, expected=%f' % (yhat, obs))
+    # evaluate forecasts
+    mse = mean_squared_error(x_valid, predictions)
+    print('Test MSE: %.3f' % mse)
+    # plot forecasts against actual outcomes
+    plt.plot(x_valid)
+    plt.plot(predictions, color='red')
+    plt.show()   
+    
+#TF Data prep function
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+    
+#Keras LSTM model
+def lstm_model():
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1),
+                          input_shape=[None]),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32, return_sequences=True)),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+      tf.keras.layers.Dense(1),
+      tf.keras.layers.Lambda(lambda x: x * 100.0)
+        ])
+    model.compile(loss=tf.keras.losses.Huber(),
+              optimizer=optimizer,
+              metrics=["mse"])
+    return model
+
+#Callback Functions
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('val_mse')<21.882):
+            print("\n MSE Reached less than benchmark model so cancelling training!")
+            self.model.stop_training = True
+
+def plot_metric(history, metric,zoom_range):
+    train_metrics = history.history[metric]
+    val_metrics = history.history['val_'+metric]
+    epochs = range(1, len(train_metrics) + 1)
+    plt.plot(epochs, train_metrics)
+    plt.plot(epochs, val_metrics)
+    plt.title('Training and validation '+ metric)
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend(["train_"+metric, 'val_'+metric])
+    plt.axis(zoom_range)
+    plt.show()
